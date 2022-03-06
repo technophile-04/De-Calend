370a47572586f0438255edc19e281340990dc70e
Shiv Bhonde <shivbhonde04@gmail.com>
2022-03-06 09:44:35 +0530

    create basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { useEffect, useState } from 'react';
+import Calender from './components/Calender';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [account, setAccount] = useState('');
+
+	const handleClick = async () => {
+		try {
+			const provider = await detectEthereumProvider();
+
+			const accounts = await provider.request({
+				method: 'eth_requestAccounts',
+			});
+
+			if (accounts.length > 0) {
+				setAccount(accounts[0]);
+				console.log('account found', accounts);
+			} else {
+				console.log('No account found');
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const isConnected = async () => {
+		const provider = await detectEthereumProvider();
+		if (provider) {
+			const accounts = await provider.request({ method: 'eth_accounts' });
+			if (accounts.length > 0) {
+				setAccount(accounts[0]);
+			} else {
+				console.log('NO authorized account found');
+			}
+		} else {
+			console.log('Please install Metamask');
+		}
+	};
+
+	useEffect(() => {
+		isConnected();
+	}, []);
+
+	return (
+		<div>
+			<h1 className="text-4xl font-bold">DeCalend</h1>
+			<p className="text-2xl">Web 3 appointment scheduler</p>
+			{!account && (
+				<button
+					onClick={handleClick}
+					className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+				>
+					Connect Wallet
+				</button>
+			)}
+			{account && <Calender />}
+		</div>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/Calender.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Calender = () => {
+	return (
+		<div className="h-[600px] m-5 text-black bg-white text-2xl">Calender</div>
+	);
+};
+
+export default Calender;
